36fd74c2fd02ee18bc7d56e9970ccdfaa76e0ef7
SilverBell17 <kevinjoun17@gmail.com>
2019-06-12 22:39:07 -0700

    adding code so that a random letter is generated on game reset.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,22 +2,24 @@ var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var randomLetter = letters[Math.floor(Math.random() * letters.length)];
-console.log(randomLetter);
+var randomLetter = 1
+function random(){
+    return letters[Math.floor(Math.random() * letters.length)];
+}
+randomLetter = random();
 
 
 document.onkeyup = function(event) {
     var userChoice = event.key;
-    console.log(userChoice);
     if (userChoice !== randomLetter){  //if user choice = the randomly generated number
         guessesLeft--; //reduce the guessesLeft by 1.
-        console.log(guessesLeft);
         document.getElementById("guessedSoFar").append(" " + userChoice.toUpperCase());
     } else if (userChoice === randomLetter){
         wins++;
         guessesLeft = 10;
         document.getElementById("guessedSoFar").innerHTML = ("Your Guesses so far:");
         document.getElementById("wins").innerHTML = "Wins: " + wins;
+        randomLetter = random();
         //Some code to recreate a random number for randomLetter variable. So that the game can reset.
     }
 
@@ -26,6 +28,7 @@ document.onkeyup = function(event) {
         guessesLeft = 10;
         document.getElementById("guessedSoFar").innerHTML = ("Your Guesses so far:");
         document.getElementById("losses").innerHTML ="Losses: " + losses;
+        randomLetter = random();
     }
 
     document.getElementById("guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
